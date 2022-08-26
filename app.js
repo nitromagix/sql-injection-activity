0bbf27d60affcbc2a27363ca97504376ee572bb7
Martin <nitromagix@gmail.com>
2022-08-25 18:04:27 -0700

    added form; sqlite table

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.use(express.static("."));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// const db = new sqlite3.Database("memory:");
+// db.serialize(() => {
+//   db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
+//   db.run(
+//     "INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')"
+//   );
+//   console.log("sqlite3 'memory' database and 'user' table successfully created")
+// });
+
 app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
