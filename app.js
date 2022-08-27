8aa4ae610c2fb2feea8cdd2712c3739959321639
Martin <nitromagix@gmail.com>
2022-08-27 15:33:00 -0700

    finished assignment

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.use(express.static("."));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// const db = new sqlite3.Database("memory:");
+const db = new sqlite3.Database("memory:");
+
 // db.serialize(() => {
 //   db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
 //   db.run(
@@ -21,6 +22,33 @@ app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.sendFile("index.html");
+});
+
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  // console.log(username, password);
+  let query = `SELECT title FROM user WHERE username ='${username}' AND password ='${password}'`;
+
+  db.get(query, function (err, row) {
+    if (err) {
+      console.log("ERROR", err);
+      res.redirect("/index.html#error");
+    } else if (!row) {
+      res.redirect("/index.html#unauthorized");
+    } else {
+      res.send(
+        `Hello <b>` +
+          row.title +
+          `!</b><br /> 
+      This file contains all your secret data: <br /><br /> 
+      SECRETS <br /><br /> MORE SECRETS <br /><br /> 
+      <a href="/index.html">Go back to login</a>`
+      );
+    }
+  });
+
+  // res.status(200).send("OK");
 });
 
 app.listen(3333, () => {
